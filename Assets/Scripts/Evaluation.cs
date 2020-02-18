f000b8ae94bf69f67cdb52f8c05f5c1c12807fa3
CatarinaFidalgo <59732505+CatarinaFidalgo@users.noreply.github.com>
2020-02-18 18:39:32 +0000

    Remote Final

---
+++ Assets/Scripts/Evaluation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public enum ConditionType
+{
+    Approach,
+    Veridical,
+    SideToSide
+}
+
+
+
+public class Evaluation : MonoBehaviour
+{
+
+    public ConditionType condition;
+
+    public Transform localWorkspace;
+    public Transform remoteWorkspace;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+
+
+        
+    }
+}
