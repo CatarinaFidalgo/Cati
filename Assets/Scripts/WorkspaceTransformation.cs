a98ecf72744648bacdee758c37e9dfc19c567534
CatarinaFidalgo <59732505+CatarinaFidalgo@users.noreply.github.com>
2020-02-25 19:41:32 +0000

    25/05

--- Assets/Scripts/WorkspaceTransformation.cs
+++ Assets/Scripts/WorkspaceTransformation.cs
@@ -7,6 +7,9 @@ public class WorkspaceTransformation : MonoBehaviour
 {
 
 
+    public bool inWorkspace;
+    public BoxCollider volumeCollider;
+
     public Evaluation evaluation;
 
     public Transform remoteLeft;
@@ -18,7 +21,9 @@ public class WorkspaceTransformation : MonoBehaviour
 
     public Transform remoteRight;
     public Transform warpedHandRight;
-    
+
+    public Transform newPivotLeft;
+    public Transform targetLeft;
 
     void Start()
     {
@@ -28,10 +33,25 @@ public class WorkspaceTransformation : MonoBehaviour
 
     void Update()
     {
-
-        if (evaluation.condition == ConditionType.Approach)
+        if (volumeCollider.bounds.Contains(remoteLeft.position) || volumeCollider.bounds.Contains(remoteRight.position))
         {
+           inWorkspace = true;
+        }
+        else
+        {           
+            inWorkspace = false;
+        }
+
+
+        if (evaluation.condition == ConditionType.Approach && inWorkspace == true)
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            transform.localScale = new Vector3(-1, 1, 1);
+
             warpedHandLeft.localPosition = _invert(remoteRight.localPosition);
+            warpedHandLeft.localRotation = remoteRight.localRotation;
+
+           // warpedHandLeft.LookAt(targetLeft.position - (-newPivotLeft.right), newPivotLeft.forward);
 
             //Vector3 d = handLeftTip.position - remoteLeft.position;
             //warpedHandLeft.localPosition = warpedHandLeft.localPosition - d;
@@ -39,25 +59,38 @@ public class WorkspaceTransformation : MonoBehaviour
 
 
             warpedHandRight.localPosition = _invert(remoteLeft.localPosition);
+            warpedHandRight.localRotation = remoteLeft.localRotation;
 
 
+            //TIP.position = handLeftTip.position;
+            //TIP.rotation = handLeftTip.rotation;
 
-            // if dentro troce
-            // else destroca
-
-
-            TIP.position = handLeftTip.position;
-            TIP.rotation = handLeftTip.rotation;
-
-
-
-
-
+            Debug.Log("Approach in workspace");
 
 
         }
+
+        else if (evaluation.condition == ConditionType.Approach && inWorkspace == false)
+        {
+
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            transform.localScale = new Vector3(1, 1, 1);
+
+            warpedHandLeft.localPosition = remoteLeft.localPosition;
+            warpedHandLeft.localRotation = remoteLeft.localRotation;
+
+            warpedHandRight.localPosition = remoteRight.localPosition;
+            warpedHandRight.localRotation = remoteRight.localRotation;
+
+            Debug.Log("Approach out workspace");
+
+        }
+
+
         else  // VERIDICAL
         {
+           // Debug.Log("Veridical");
+
             warpedHandLeft.localPosition = remoteLeft.localPosition; 
             warpedHandLeft.localRotation = remoteLeft.localRotation;
 
