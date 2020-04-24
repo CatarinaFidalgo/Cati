9399260e5a6efb08e72cd46dc7ae84aae00ff8dc
CatarinaFidalgo <59732505+CatarinaFidalgo@users.noreply.github.com>
2020-04-24 21:57:24 +0100

    V20

--- Assets/Scripts/WorkspaceTransformation.cs
+++ Assets/Scripts/WorkspaceTransformation.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 
 public class WorkspaceTransformation : MonoBehaviour
 {
-
-
     private bool inWorkspace;
     public BoxCollider volumeCollider;
     [Space(8)]
@@ -36,67 +34,59 @@ public class WorkspaceTransformation : MonoBehaviour
     [Space(8)]
     public Transform targetLeft; // targets are the tips of the local avatar hands calculated from the remote hands
     public Transform targetRight;
-   
+
+    /*  
        
     [Header("Right Arm Rig")]
 
     [Header("Stretch the Arms")]
 
-    public Transform shoulderRight;
+    //public Transform shoulderRight;
     public Transform elbowRight;
     public Transform wristRight;
     public Vector3 differenceAvatarWristToWarpedCRight;
     [Header("Left Arm Rig")]
    
-    public Transform shoulderLeft;
+    //public Transform shoulderLeft;
     public Transform elbowLeft;
     public Transform wristLeft;
-    public Vector3 differenceAvatarWristToWarpedCLeft;
-
-
-    private Vector3 transformLeftHandVector;
-    private Vector3 transformRightHandVector;
-
-    // reset values
-    private Transform warpedHMD_reset;
-    private Transform warpedHandRight_reset;
-    private Transform warpedHandLeft_reset;
-    private Transform warpedFingertipRight_reset;
-    private Transform warpedFingertipLeft_reset;
+    public Vector3 differenceAvatarWristToWarpedCLeft;*/
 
     //values for internal use
-    private Quaternion _lookRotation;
-    private Vector3 _direction;
+    private Vector3 transformLeftHandVector;
+    private Vector3 transformRightHandVector;
+    private float offset;
 
     private void Start()
     {
-        
+        offset = 0.4f;
     }
 
     void Update()
     {
 
-        inWorkspace = volumeCollider.bounds.Contains(remoteLeft.position) || volumeCollider.bounds.Contains(remoteRight.position);
+        inWorkspace = volumeCollider.bounds.Contains(remoteFingertipRight.position) || volumeCollider.bounds.Contains(remoteFingertipLeft.position);
 
 
-        if (evaluation.condition == ConditionType.Approach)
+        if (evaluation.condition == ConditionType.Approach && inWorkspace)
         {
-            
+
             warpedSpace.localScale = new Vector3(-1, 1, 1);
 
-            //Mirror hands and put them in the right place
+            //Mirror hands and put them in the right place            
 
-            warpedHMD.localPosition = remoteHMD.localPosition;
+            warpedHMD.localPosition = new Vector3(remoteHMD.localPosition.x, remoteHMD.localPosition.y, -remoteHMD.localPosition.z - offset);
+            //warpedHMD.localPosition = remoteHMD.localPosition;
             warpedHandRight.localPosition = remoteLeft.localPosition;
             warpedHandLeft.localPosition = remoteRight.localPosition;
             warpedFingertipRight.localPosition = remoteFingertipLeft.localPosition;
-            warpedFingertipLeft.localPosition = remoteFingertipRight.localPosition;            
+            warpedFingertipLeft.localPosition = remoteFingertipRight.localPosition;
 
-            
+
             warpedHMD.localRotation = remoteHMD.localRotation;
             warpedHandRight.localRotation = remoteLeft.localRotation;
-            warpedHandLeft.localRotation = remoteRight.localRotation;           
-            warpedFingertipRight.localRotation = remoteFingertipLeft.localRotation;            
+            warpedHandLeft.localRotation = remoteRight.localRotation;
+            warpedFingertipRight.localRotation = remoteFingertipLeft.localRotation;
             warpedFingertipLeft.localRotation = remoteFingertipRight.localRotation;
 
             //Calculate the local tip from the remote tip coordinates
@@ -109,61 +99,32 @@ public class WorkspaceTransformation : MonoBehaviour
             transformRightHandVector = targetLeft.position - warpedFingertipRight.position;
             transformLeftHandVector = targetRight.position - warpedFingertipLeft.position;
 
-
             //1st Update of the warped hands position with the transform vector 1
 
-            warpedHandLeft.position = warpedHandLeft.position + transformLeftHandVector; 
-            warpedHandRight.position = warpedHandRight.position + transformRightHandVector;
+            warpedHandLeft.position += transformLeftHandVector;
+            warpedHandRight.position += transformRightHandVector;
 
-            /* //Arm Stretching
-
-             differenceAvatarWristToWarpedCLeft = new Vector3 (0, 0.1f, 0.1f);
-
-
-             if (!Input.GetKeyDown(KeyCode.Space)) //!OVRInput.GetDown(OVRInput.Button.One)
-             {
-                 differenceAvatarWristToWarpedCLeft = Vector3.zero;
-                 differenceAvatarWristToWarpedCRight = Vector3.zero;
-                 //Debug.Log("PRESSED NOTHING");
-
-             }            
-
-             if (Input.GetKeyDown(KeyCode.Space)) //Press a button to let the system know it is allright to start taking measures
-             {
-                 differenceAvatarWristToWarpedCLeft = (warpedHandLeft.position - wristLeft.position);
-                 differenceAvatarWristToWarpedCRight = (warpedHandRight.position - wristRight.position);
-                 Debug.Log("PRESSED A");
-             }
-
-             if ((warpedHandLeft.position - wristLeft.position).magnitude > differenceAvatarWristToWarpedCLeft.magnitude)
-             {
-                 differenceAvatarWristToWarpedCLeft = (warpedHandLeft.position - wristLeft.position);
-                 Debug.Log("Increased the stretching on the Left");
-             }
-
-             if ((warpedHandRight.position - wristRight.position).magnitude > differenceAvatarWristToWarpedCRight.magnitude)
-             {
-                 differenceAvatarWristToWarpedCRight = (warpedHandRight.position - wristRight.position);
-                 Debug.Log("Increased the stretching on the Right");
-             }
-
-             // Debug.DrawLine(warpedHandLeft.position, warpedHandLeft.position + differenceAvatarWristToWarpedCLeft, Color.green);
-
-
-             shoulderLeft.localPosition = shoulderLeft.localPosition + new Vector3 (0, 0.5f*differenceAvatarWristToWarpedCLeft.z, 0);    //Porque só podemos mexer em z para alongar       
-             elbowLeft.localPosition = elbowLeft.localPosition + new Vector3(0, 0.5f * differenceAvatarWristToWarpedCLeft.z, 0);
-
-             //shoulderRight.localPosition = shoulderRight.localPosition + new Vector3(0, 0.5f * differenceAvatarWristToWarpedCRight.z, 0);            
-            // elbowRight.localPosition = elbowRight.localPosition + new Vector3(0, 0.5f * differenceAvatarWristToWarpedCRight.z, 0);
-
-             //2nd Update of the warped hands position with the transform vector 2
-
-             warpedHandLeft.position = warpedHandLeft.position + differenceAvatarWristToWarpedCLeft;
-             //warpedHandRight.position = warpedHandRight.position + differenceAvatarWristToWarpedCRight;
-             */
         }
 
+        else if (evaluation.condition == ConditionType.Approach && !inWorkspace)
+        {
+            warpedSpace.localScale = new Vector3(-1, 1, 1);
 
+            //Mirror hands and put them in the right place
+
+            warpedHMD.localPosition = remoteHMD.localPosition;
+            warpedHandRight.localPosition = remoteLeft.localPosition;
+            warpedHandLeft.localPosition = remoteRight.localPosition;
+            warpedFingertipRight.localPosition = remoteFingertipLeft.localPosition;
+            warpedFingertipLeft.localPosition = remoteFingertipRight.localPosition;
+
+
+            warpedHMD.localRotation = remoteHMD.localRotation;
+            warpedHandRight.localRotation = remoteLeft.localRotation;
+            warpedHandLeft.localRotation = remoteRight.localRotation;
+            warpedFingertipRight.localRotation = remoteFingertipLeft.localRotation;
+            warpedFingertipLeft.localRotation = remoteFingertipRight.localRotation;
+        }
 
 
         if (evaluation.condition == ConditionType.Veridical || evaluation.condition == ConditionType.SideToSide)
@@ -173,13 +134,14 @@ public class WorkspaceTransformation : MonoBehaviour
             warpedHMD.localPosition = remoteHMD.localPosition;
             warpedHMD.localRotation = remoteHMD.localRotation;
 
-            warpedHandLeft.localPosition = remoteLeft.localPosition; 
+            warpedHandLeft.localPosition = remoteLeft.localPosition;
             warpedHandLeft.localRotation = remoteLeft.localRotation;
 
-            warpedHandRight.localPosition = remoteRight.localPosition; 
-            warpedHandRight.localRotation = remoteRight.localRotation;         
-            
+            warpedHandRight.localPosition = remoteRight.localPosition;
+            warpedHandRight.localRotation = remoteRight.localRotation;
+
         }
     }
-    
+
 }
+
